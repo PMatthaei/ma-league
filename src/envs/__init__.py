627e626da5392335c267290cc003943f1b07292c
PMatthaei <pmd.matthaei@gmail.com>
2021-02-12 17:39:22 +0100

    Fixed stats logging

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,8 +1,9 @@
+import logging
 from functools import partial
 
 from smac.env import MultiAgentEnv, StarCraft2Env
 
-from multiagent.environment import MultiAgentEnv as MAEnv
+from multiagent.environment import MAEnv
 from multiagent.scenarios import team
 from bin.team_plans_example import TWO_TEAMS_SIZE_TWO_SYMMETRIC_HETEROGENEOUS
 
@@ -21,17 +22,18 @@ if sys.platform == "linux":
 
 def ma_env(env, **kwargs) -> MAEnv:
     # load scenario from script
+    # TODO: init via kwargs from yaml
     scenario = team.load('teams.py').TeamsScenario(TWO_TEAMS_SIZE_TWO_SYMMETRIC_HETEROGENEOUS)
     # create world
     world = scenario.make_teams_world(grid_size=10.0)
     # create multi-agent environment
-    return MAEnv(world=world,
+    return env(world=world,
                  reset_callback=scenario.reset_world,
                  reward_callback=scenario.reward,
                  observation_callback=scenario.observation,
                  info_callback=None,
                  done_callback=scenario.done,
-                 log=False)
+                 log=True, log_level=logging.ERROR)
 
 
 REGISTRY = {}
