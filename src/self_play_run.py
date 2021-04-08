f241a160f5706950347597049dc0b7d1b156d510
PMatthaei <pmd.matthaei@gmail.com>
2021-04-08 09:43:17 +0200

    League
    - Moved SelfPlay to class to manage more properties
    - Improved checkpointing and model load/save

--- src/self_play_run.py
+++ src/self_play_run.py
@@ -3,12 +3,10 @@ import os
 import pprint
 import time
 import threading
-from multiprocessing.connection import Connection
 
 import torch as th
 from types import SimpleNamespace as SN
 
-from league.roles.players import Player
 from runners.self_play_runner import SelfPlayRunner
 from utils.checkpoint_manager import CheckpointManager
 from utils.logging import LeagueLogger
@@ -50,7 +48,8 @@ def run(_run, _config, _log):
     logger.setup_sacred(_run)
 
     # Run and train
-    run_sequential(args=args, logger=logger)
+    selfplay = SelfPlayRun(args=args, logger=logger)
+    selfplay.run()
 
     # Clean up after finishing
     print("Exiting Main")
@@ -68,154 +67,164 @@ def run(_run, _config, _log):
     os._exit(os.EX_OK)
 
 
-def evaluate_sequential(args, runner):
-    for _ in range(args.test_nepisode):
-        runner.run(test_mode=True)
+class SelfPlayRun:
 
-    if args.save_replay:
-        runner.save_replay()
+    def __init__(self, args, logger):
+        self.args = args
+        self.logger = logger
 
-    runner.close_env()
+        # Init runner so we can get env info
+        self.runner = SelfPlayRunner(args=args, logger=logger)
 
+        # Set up schemes and groups here
+        env_info = self.runner.get_env_info()
+        args.n_agents = int(env_info["n_agents"] / 2)  # TODO: assuming same team size
+        args.n_actions = env_info["n_actions"]
+        args.state_shape = env_info["state_shape"]
 
-def run_sequential(args, logger):
-    # Init runner so we can get env info
-    runner = SelfPlayRunner(args=args, logger=logger)
+        # Default/Base scheme
+        scheme = {
+            "state": {"vshape": env_info["state_shape"]},
+            "obs": {"vshape": env_info["obs_shape"], "group": "agents"},
+            "actions": {"vshape": (1,), "group": "agents", "dtype": th.long},
+            "avail_actions": {"vshape": (env_info["n_actions"],), "group": "agents", "dtype": th.int},
+            "reward": {"vshape": (1,)},
+            "terminated": {"vshape": (1,), "dtype": th.uint8},
+        }
+        groups = {
+            "agents": args.n_agents
+        }
+        preprocess = {
+            "actions": ("actions_onehot", [OneHot(out_dim=args.n_actions)])
+        }
 
-    # Set up schemes and groups here
-    env_info = runner.get_env_info()
-    args.n_agents = int(env_info["n_agents"] / 2)  # TODO: assuming same team size
-    args.n_actions = env_info["n_actions"]
-    args.state_shape = env_info["state_shape"]
+        # Buffers
+        self.opponent_buffer = ReplayBuffer(scheme, groups, args.buffer_size, env_info["episode_limit"] + 1,
+                                            preprocess=preprocess,
+                                            device="cpu" if args.buffer_cpu_only else args.device)
 
-    # Default/Base scheme
-    scheme = {
-        "state": {"vshape": env_info["state_shape"]},
-        "obs": {"vshape": env_info["obs_shape"], "group": "agents"},
-        "actions": {"vshape": (1,), "group": "agents", "dtype": th.long},
-        "avail_actions": {"vshape": (env_info["n_actions"],), "group": "agents", "dtype": th.int},
-        "reward": {"vshape": (1,)},
-        "terminated": {"vshape": (1,), "dtype": th.uint8},
-    }
-    groups = {
-        "agents": args.n_agents
-    }
-    preprocess = {
-        "actions": ("actions_onehot", [OneHot(out_dim=args.n_actions)])
-    }
+        self.home_buffer = ReplayBuffer(scheme, groups, args.buffer_size, env_info["episode_limit"] + 1,
+                                        preprocess=preprocess,
+                                        device="cpu" if args.buffer_cpu_only else args.device)
 
-    # Buffers
-    opponent_buffer = ReplayBuffer(scheme, groups, args.buffer_size, env_info["episode_limit"] + 1,
-                                   preprocess=preprocess,
-                                   device="cpu" if args.buffer_cpu_only else args.device)
+        # Setup multi-agent controller here
+        self.home_mac = mac_REGISTRY[args.mac](self.home_buffer.scheme, groups, args)
+        self.opponent_mac = mac_REGISTRY[args.mac](self.opponent_buffer.scheme, groups, args)
 
-    home_buffer = ReplayBuffer(scheme, groups, args.buffer_size, env_info["episode_limit"] + 1,
-                               preprocess=preprocess,
-                               device="cpu" if args.buffer_cpu_only else args.device)
+        # Give runner the scheme
+        self.runner.setup(scheme=scheme, groups=groups, preprocess=preprocess, home_mac=self.home_mac,
+                          opponent_mac=self.opponent_mac)
 
-    # Setup multi-agent controller here
-    home_mac = mac_REGISTRY[args.mac](home_buffer.scheme, groups, args)
-    opponent_mac = mac_REGISTRY[args.mac](opponent_buffer.scheme, groups, args)
+        # Learners
+        self.home_learner = le_REGISTRY[args.learner](self.home_mac, self.home_buffer.scheme, logger, args, name="home")
+        self.opponent_learner = le_REGISTRY[args.learner](self.opponent_mac, self.opponent_buffer.scheme, logger, args,
+                                                          name="opponent")
 
-    # Give runner the scheme
-    runner.setup(scheme=scheme, groups=groups, preprocess=preprocess, home_mac=home_mac, opponent_mac=opponent_mac)
+        # Activate CUDA mode if supported
+        if args.use_cuda:
+            self.home_learner.cuda()
+            self.opponent_learner.cuda()
 
-    # Learners
-    home_learner = le_REGISTRY[args.learner](home_mac, home_buffer.scheme, logger, args, name="home")
-    opponent_learner = le_REGISTRY[args.learner](opponent_mac, opponent_buffer.scheme, logger, args, name="opponent")
+        self.checkpoint_manager = CheckpointManager(args=args, logger=logger)
 
-    # Activate CUDA mode if supported
-    if args.use_cuda:
-        home_learner.cuda()
-        opponent_learner.cuda()
+    def evaluate_sequential(self):
+        for _ in range(self.args.test_nepisode):
+            self.runner.run(test_mode=True)
 
-    checkpoint_manager = CheckpointManager(args=args, logger=logger)
+        if self.args.save_replay:
+            self.runner.save_replay()
 
-    if args.checkpoint_path != "":
-        timestep_to_load = checkpoint_manager.load(learners=[home_learner, opponent_learner])
-        runner.t_env = timestep_to_load
+        self.runner.close_env()
 
-        if args.evaluate or args.save_replay:
-            evaluate_sequential(args, runner)
-            return
+    def run(self):
+        if self.args.checkpoint_path != "":
+            timestep_to_load = self.checkpoint_manager.load(learners=[self.home_learner, self.opponent_learner])
+            self.runner.t_env = timestep_to_load
 
-    # start training
-    episode = 0
-    last_test_T = -args.test_interval - 1
-    last_log_T = 0
-    model_save_time = 0
+            if self.args.evaluate or self.args.save_replay:
+                self.evaluate_sequential()
+                return
 
-    start_time = time.time()
-    last_time = start_time
+        # start training
+        episode = 0
+        last_test_T = -self.args.test_interval - 1
+        last_log_T = 0
+        model_save_time = 0
 
-    logger.console_logger.info("Beginning training for {} timesteps".format(args.t_max))
+        start_time = time.time()
+        last_time = start_time
 
-    #
-    #
-    # Main Loop
-    #
-    #
-    while runner.t_env <= args.t_max:
-
-        # Run for a whole episode at a time
-        home_batch, opponent_batch = runner.run(test_mode=False)
-
-        home_buffer.insert_episode_batch(home_batch)
-        opponent_buffer.insert_episode_batch(opponent_batch)
-
-        # Sample batch from buffer if possible
-        if home_buffer.can_sample(args.batch_size) and opponent_buffer.can_sample(args.batch_size):
-            home_sample = home_buffer.sample(args.batch_size)
-            opponent_sample = opponent_buffer.sample(args.batch_size)
-
-            # Truncate batch to only filled timesteps
-            max_ep_t_h = home_sample.max_t_filled()
-            max_ep_t_o = opponent_sample.max_t_filled()
-            home_sample = home_sample[:, :max_ep_t_h]
-            opponent_sample = opponent_sample[:, :max_ep_t_o]
-
-            if home_sample.device != args.device:
-                home_sample.to(args.device)
-
-            if opponent_sample.device != args.device:
-                opponent_sample.to(args.device)
-
-            home_learner.train(home_sample, runner.t_env, episode)
-            opponent_learner.train(opponent_sample, runner.t_env, episode)
+        self.logger.console_logger.info("Beginning training for {} timesteps".format(self.args.t_max))
 
         #
-        # Execute test runs once in a while
         #
-        n_test_runs = max(1, args.test_nepisode // runner.batch_size)
-        if (runner.t_env - last_test_T) / args.test_interval >= 1.0:
+        # Main Loop
+        #
+        #
+        while self.runner.t_env <= self.args.t_max:
 
-            logger.console_logger.info("t_env: {} / {}".format(runner.t_env, args.t_max))
-            logger.console_logger.info("Estimated time left: {}. Time passed: {}".format(
-                time_left(last_time, last_test_T, runner.t_env, args.t_max), time_str(time.time() - start_time)))
-            last_time = time.time()
+            # Run for a whole episode at a time
+            home_batch, opponent_batch = self.runner.run(test_mode=False)
 
-            last_test_T = runner.t_env
-            for _ in range(n_test_runs):
-                runner.run(test_mode=True)
+            self.home_buffer.insert_episode_batch(home_batch)
+            self.opponent_buffer.insert_episode_batch(opponent_batch)
 
-        # Model saving
-        if args.save_model and (runner.t_env - model_save_time >= args.save_model_interval or model_save_time == 0):
-            model_save_time = runner.t_env
-            checkpoint_manager.save(model_save_time, learners=[home_learner, opponent_learner])
+            # Sample batch from buffer if possible
+            batch_size = self.args.batch_size
+            if self.home_buffer.can_sample(batch_size) and self.opponent_buffer.can_sample(batch_size):
+                home_sample = self.home_buffer.sample(batch_size)
+                opponent_sample = self.opponent_buffer.sample(batch_size)
 
-        # Batch size == how many episodes are run -> add on top of episode counter
-        episode += args.batch_size_run
+                # Truncate batch to only filled timesteps
+                max_ep_t_h = home_sample.max_t_filled()
+                max_ep_t_o = opponent_sample.max_t_filled()
+                home_sample = home_sample[:, :max_ep_t_h]
+                opponent_sample = opponent_sample[:, :max_ep_t_o]
 
-        # Log
-        if (runner.t_env - last_log_T) >= args.log_interval:
-            logger.add_stat("episode", episode, runner.t_env)
-            logger.log_recent_stats()
-            last_log_T = runner.t_env
-    #
-    #
-    #
-    #
-    #
+                device = self.args.device
+                if home_sample.device != device:
+                    home_sample.to(device)
 
-    runner.close_env()
-    logger.console_logger.info("Finished Training")
+                if opponent_sample.device != device:
+                    opponent_sample.to(device)
+
+                self.home_learner.train(home_sample, self.runner.t_env, episode)
+                self.opponent_learner.train(opponent_sample, self.runner.t_env, episode)
+
+            #
+            # Execute test runs once in a while
+            #
+            n_test_runs = max(1, self.args.test_nepisode // self.runner.batch_size)
+            if (self.runner.t_env - last_test_T) / self.args.test_interval >= 1.0:
+
+                self.logger.console_logger.info("t_env: {} / {}".format(self.runner.t_env, self.args.t_max))
+                self.logger.console_logger.info("Estimated time left: {}. Time passed: {}".format(
+                    time_left(last_time, last_test_T, self.runner.t_env, self.args.t_max),
+                    time_str(time.time() - start_time)))
+                last_time = time.time()
+
+                last_test_T = self.runner.t_env
+                for _ in range(n_test_runs):
+                    self.runner.run(test_mode=True)
+
+            # Model saving
+            if self.args.save_model and (self.runner.t_env - model_save_time >= self.args.save_model_interval or model_save_time == 0):
+                model_save_time = self.runner.t_env
+                self.checkpoint_manager.save(model_save_time, learners=[self.home_learner, self.opponent_learner])
+
+            # Batch size == how many episodes are run -> add on top of episode counter
+            episode += self.args.batch_size_run
+
+            # Log
+            if (self.runner.t_env - last_log_T) >= self.args.log_interval:
+                self.logger.add_stat("episode", episode, self.runner.t_env)
+                self.logger.log_recent_stats()
+                last_log_T = self.runner.t_env
+        #
+        #
+        #
+        #
+        #
+
+        self.runner.close_env()
+        self.logger.console_logger.info("Finished Training")
--- src/utils/checkpoint_manager.py
+++ src/utils/checkpoint_manager.py
@@ -1,37 +1,40 @@
 import os
+from os.path import abspath, dirname
 from typing import List
 
 from learners.learner import Learner
+from main import results_path
 
 
 class CheckpointManager:
 
     def __init__(self, args, logger):
-        self.local_results_path = args.local_results_path
         self.unique_token = args.unique_token
         self.checkpoint_path = args.checkpoint_path
         self.load_step = args.load_step
         self.logger = logger
         pass
 
-    def save(self, t_env, learners: List[Learner]):
-        save_path = os.path.join(self.local_results_path, "models", self.unique_token, str(t_env))
+    def save(self, t_env, learners: List[Learner]) -> str:
+        save_path = os.path.join(results_path, "models", self.unique_token, str(t_env))
         os.makedirs(save_path, exist_ok=True)
         self.logger.console_logger.info("Saving models to {}".format(save_path))
 
         [learner.save_models(save_path, learner.name) for learner in learners]
+        return save_path
 
-    def load(self, learners: List[Learner]) -> int:
+    def load(self, learners: List[Learner], checkpoint_path=None) -> int:
+        path = checkpoint_path if checkpoint_path is not None else self.checkpoint_path
         timesteps = []
         timestep_to_load = 0
 
-        if not os.path.isdir(self.checkpoint_path):
-            self.logger.console_logger.info("Checkpoint directory {} doesn't exist".format(self.checkpoint_path))
+        if not os.path.isdir(path):
+            self.logger.console_logger.info("Checkpoint directory {} doesn't exist".format(path))
             return -1
 
         # Go through all files in args.checkpoint_path
-        for name in os.listdir(self.checkpoint_path):
-            full_name = os.path.join(self.checkpoint_path, name)
+        for name in os.listdir(path):
+            full_name = os.path.join(path, name)
             # Check if they are dirs the names of which are numbers
             if os.path.isdir(full_name) and name.isdigit():
                 timesteps.append(int(name))
@@ -43,7 +46,7 @@ class CheckpointManager:
             # choose the timestep closest to load_step
             timestep_to_load = min(timesteps, key=lambda x: abs(x - self.load_step))
 
-        model_path = os.path.join(self.checkpoint_path, str(timestep_to_load))
+        model_path = os.path.join(path, str(timestep_to_load))
 
         self.logger.console_logger.info("Loading model from {}".format(model_path))
         [learner.save_models(model_path, learner.name) for learner in learners]
