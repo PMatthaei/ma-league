7732e6968b570f9e8107172a98ceb635082a09e8
PMatthaei <pmd.matthaei@gmail.com>
2021-04-28 16:48:10 +0200

    Cleanup
    - Cleaned requirements.txt
    - Removed SMAC env
    - Cleaned Dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo 'Building Dockerfile with image name pymarl:1.0'
+echo 'Building Image "ma-league:1.0" from Dockerfile.'
+# Copy the requirements file to reproduce environment in docker image
 cp ../requirements.txt .
 docker build -t ma-league:1.0 .
--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -4,20 +4,10 @@ import sys
 import os
 
 from multiagent.environment import TeamsEnv
-from smac.env import StarCraft2Env
-
-if sys.platform == "linux":
-    os.environ.setdefault("SC2PATH", os.path.join(os.getcwd(), "3rdparty", "StarCraftII"))
-
-
-def smac_env(env, **kwargs) -> StarCraft2Env:
-    return env(**kwargs)
 
 
 def ma_env(env, **kwargs) -> TeamsEnv:
     return env(**kwargs)
 
 
-REGISTRY = {}
-REGISTRY["sc2"] = partial(smac_env, env=StarCraft2Env)
-REGISTRY["ma"] = partial(ma_env, env=TeamsEnv)
+REGISTRY = {"ma": partial(ma_env, env=TeamsEnv)}
