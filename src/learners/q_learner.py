be58829fd9fae1433fdd3262c89bb76bb363de87
PMatthaei <pmd.matthaei@gmail.com>
2021-05-05 12:27:04 +0200

    Fixed error with dict loading

--- src/learners/q_learner.py
+++ src/learners/q_learner.py
@@ -126,7 +126,7 @@ class QLearner(Learner):
     def _update_targets(self):
         self.target_mac.load_state(self.mac)
         if self.mixer is not None:
-            self.target_mixer.load_state_dict(th.load(self.mixer.state_dict()))
+            self.target_mixer.load_state_dict(self.mixer.state_dict())
         self.logger.console_logger.info("Updated {0}target network.".format(self.name))
 
     def cuda(self):
