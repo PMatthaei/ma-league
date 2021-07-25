e48c5978869132a1e529a89208ea52c0dfcd1082
PMatthaei <pmd.matthaei@gmail.com>
2021-07-25 21:50:46 +0200

    League Rework
    - Support for ensemble experiment

--- src/league/processes/training/ensemble_league_process.py
+++ src/league/processes/training/ensemble_league_process.py
@@ -34,11 +34,12 @@ class EnsembleLeagueProcess(LeagueExperimentProcess):
 
     @property
     def ensemble_agent_state(self) -> OrderedDict:
-        return self._experiment.home_mac.ensemble[0].state_dict() # TODO: how get varius ensemble not just first
+        return self._experiment.home_mac.ensemble[0].state_dict()  # TODO: how get various ensemble not just first
 
     def _run_experiment(self) -> None:
-
+        #
         # Initial play to train policy of the team against AI against mirrored team -> Performed for each team
+        #
         self._logger.info(f"Start training in process: {self._proc_id} with {self._home_team}")
         self._configure_experiment(home=self._home_team, ai=True)
         self._experiment = MultiAgentExperiment(args=self._args, logger=self._logger)
@@ -46,47 +47,56 @@ class EnsembleLeagueProcess(LeagueExperimentProcess):
         self._experiment.start(play_time_seconds=self._args.league_play_time_mins * 60)
         self._logger.info(f"Share agent from process: {self._proc_id}")
         self._share_agent_params(agent=self.home_agent_state)  # make agent accessible to other instances
-        self._native_agent: OrderedDict = copy.deepcopy(self.home_agent_state)  # save an instance of the original agent
 
         start_time = time.time()
         end_time = time.time()
 
+        #
+        # Training loop
+        #
         while end_time - start_time <= self._args.league_runtime_hours * 60 * 60:
+            #
+            # Retrieve current version of the agent from the pool
+            #
+            home_team, agent_state = self.shared_agent
 
+            #
             # Fetch agents from another teams training instance
+            #
             foreign: Tuple[Team, OrderedDict] = self._matchmaking.get_match(self._home_team)
             if foreign is None:
                 self._logger.info(f"No match found. Ending process: {self._proc_id} with {self._home_team}")
                 break
+            self._away_team, foreign_agent_state = foreign  # Note: Set away team for payoff tensor
+            self._logger.info(f"Matched foreign team {self._away_team.id_} in process: {self._proc_id}")
 
-            foreign_team, foreign_agent = foreign
-            self._logger.info(f"Matched foreign team {foreign_team.id_} in process: {self._proc_id}")
-
+            #
+            # Evaluate how the agent performs in an ensemble with the foreign agent (and its team constellation)
+            #
             self._logger.info(f"Build foreign team play in process: {self._proc_id}")
-            self._configure_experiment(home=foreign_team, ai=True)  # Set the foreign team constellation as home team
-            self._experiment = EnsembleExperiment(args=self._args, logger=self._logger)
-            # Train the native agent in a different team setup as foreign agent
-            self._experiment.load_ensemble(native=foreign_agent, foreign_agent=self._native_agent)
-            # Evaluate how good the mixed team performs
+            self._configure_experiment(home=self._away_team, ai=True)  # Set the foreign team constellation as home team
+            self._experiment = EnsembleExperiment(args=self._args, logger=self._logger, on_episode_end=self._send_result)
+            self._experiment.load_ensemble(native=foreign_agent_state, foreign_agent=agent_state)
             self._logger.info(f"Evaluate ensemble in process: {self._proc_id}")
             self._experiment.evaluate_sequential(test_n_episode=self._args.n_league_evaluation_episodes)
 
-            # Train only new foreign agent with the team performing as before
-            self._args.freeze_native = True  # Freeze weights of native agent
+            #
+            # Train the native agent in an ensemble with the foreign agent (and its team constellation)
+            #
             self._logger.info(f"Train ensemble in process: {self._proc_id}")
-            self._configure_experiment(home=foreign_team, ai=True)  # Set the foreign team constellation as home team
-            self._experiment = EnsembleExperiment(args=self._args, logger=self._logger)
-            self._experiment.load_ensemble(native=self._native_agent, foreign_agent=foreign_agent)
+            self._configure_experiment(home=self._away_team, ai=True)  # Set the foreign team constellation as home team
+            self._experiment = EnsembleExperiment(args=self._args, logger=self._logger, on_episode_end=self._send_result)
+            self._experiment.load_ensemble(native=foreign_agent_state, foreign_agent=agent_state)
             self._experiment.start(play_time_seconds=self._args.league_play_time_mins * 60)
 
+            #
             # Share agent after training to make its current state accessible to other processes
+            #
             self._logger.info(f"Share trained ensemble in process: {self._proc_id}")
-            self._share_agent_params(team=foreign_team, agent=self.ensemble_agent_state)
-            # Select next agent to train
-            self._logger.info(f"Selected team {foreign[0].id_} for next iteration in process: {self._proc_id}")
+            self._share_agent_params(agent=self.ensemble_agent_state)
 
             end_time = time.time()
 
-        self._logger.info(f"Sampled all adversary teams in process: {self._proc_id}")
+        self._logger.info(f"Training in process finished: {self._proc_id}")
 
         self._request_close()
