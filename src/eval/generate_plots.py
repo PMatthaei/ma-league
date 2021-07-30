697134e40c20d805f8f1d14ac84111c6ddf35888
PMatthaei <pmd.matthaei@gmail.com>
2021-07-30 18:15:48 +0200

    Improved Plotting

--- src/eval/generate_plots.py
+++ src/eval/generate_plots.py
@@ -1,72 +1,77 @@
 import json
+from copy import copy
+from typing import List, Dict
+
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
-
-plot_data = {}
-metrics = []
-
-algos = []
-envs = []
-
-label = "qmix_4t_h1"
-path = "/home/pmatthaei/Projects/ma-league-results/saba/results/league_2021-07-29_22-32-44/instance_4/sacred/1/info.json"
-out_path="/home/pmatthaei/Projects/ma-league-results/saba/results/league_2021-07-29_22-32-44/instance_4/sacred/1/" + "plots/"
-
-def extract_plot_data():
-    # Read json
-    with open(path) as json_file:
-        data = json.load(json_file)
-        for metric in data:
-            # Transposed corresponds to xs values. We identify metrics by ys
-            if '_T' not in metric:
-                # If we have not already seen this metric in a json -> add to plots
-                if metric not in metrics:
-                    metrics.append(metric)
-
-                # Get data for this metric and the current experiment
-                transposed_identifier = metric + '_T'
-                xs = data[transposed_identifier]
-                ys = data[metric]
-
-                # Save the experiment data in the plot dict and provide label
-                if metric in plot_data:
-                    plot_data[metric].append((xs, ys, label))
-                else:
-                    plot_data[metric] = []
-                    plot_data[metric].append((xs, ys, label))
+import numpy as np
 
 
-# Smoothing parameters
-n = 30  # the larger n is, the smoother curve will be
-b = [1.0 / n] * n
-a = 1
-not_smooth = ['epsilon', 'td_error_abs', 'loss']
+# plot_data = {}
+# metrics = []
+#
+# algos = []
+# envs = []
+#
+# label = "qmix_4t_h1"
+# path = "/home/pmatthaei/Projects/ma-league/results/league_2021-07-30_13-00-19/instance_0/sacred/1/info.json"
+# out_path = "/home/pmatthaei/Projects/ma-league/results/league_2021-07-30_13-00-19/instance_0/sacred/1/" + "plots/"
+
+def _to_value(x):
+    return (x['value'] if 'value' in x else x['values']) if isinstance(x, dict) else x
+
+def extract_plot_data(paths: List[str]) -> Dict:
+    plot_data = {}
+    xs_original = None
+    for p in paths:
+        # Read info json
+        with open(p) as file:
+            data = json.load(file)
+            for metric in data:
+                if '_T' not in metric:  # Skip, build by ourselves
+
+                    # Get data for this metric and the current experiment
+                    transposed_identifier = f"{metric}_T"
+                    xs = data[transposed_identifier]
+                    if xs_original is None:
+                        xs_original = xs  # Used for unifying xs data
+                    ys = data[metric]
+                    ys = list(map(_to_value, ys))
+
+                    if np.all(np.isnan(ys)):
+                        continue
+                    # Save the experiment data in the plot dict and provide label
+                    if metric in plot_data:
+                        plot_data[metric]["step"].extend(xs_original[:len(ys)])
+                        plot_data[metric]["ys"].extend(ys)
+                    else:
+                        plot_data[metric] = {}
+                        plot_data[metric].update({"step": xs, "ys": ys})
+
+    return plot_data
 
 
-def plot():
-    for metric in metrics:
+def plot(data: Dict):
+    for metric, metric_data in data.items():
         # Exclude for now since weird jsons are returned
         if metric in ['home_epsilon', 'grad_norm']:
             continue
 
-        for i, data in enumerate(plot_data[metric]):
-            # If plot data is hidden in a dict property 'value' -> unpack
-            ys = list(map(lambda x: (x['value'] if 'value' in x else x['values']) if isinstance(x, dict) else x, data[1]))
-            xs = data[0]
+        df = pd.DataFrame(metric_data)
+        sns.lineplot(x="step", y="ys", ci=95, data=df, sort=True)
+        plt.ylabel(metric.replace("_", " "))
+        plt.xlim(left=-1)
+        if "percentage" in metric:
+            plt.ylim(bottom=0, top=1.0)
 
-            # Confidence interval if we have multiple values
-            sns.lineplot(x=xs, y=ys, ci=95, label=data[2])
-
-            plt.xlabel('steps')
-            plt.xlim(right=2000000)  # Experiments end at 2 million steps
-            metric_label = metric.replace('_', ' ')
-            plt.ylabel(metric_label)
-
-        plt.legend(loc='upper left')
-        plt.savefig(out_path + metric + '.png')
         plt.show()
 
 
 if __name__ == '__main__':
-    extract_plot_data()
-    plot()
+    name = "QMIX"
+    path = "/home/pmatthaei/Projects/ma-league-results/sacred/6/info.json"
+    path2 = "/home/pmatthaei/Projects/ma-league-results/sacred/5/info.json"
+    paths = [path, path2]
+    data = extract_plot_data(paths=paths)
+    plot(data)
